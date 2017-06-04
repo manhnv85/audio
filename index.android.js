1ecd0be79c502353ceddec53c49042e60b2cce8b
Manh Nguyen <manhnv@beonline.com.vn>
2017-06-04 23:18:35 +0700

    update with swiper

--- index.android.js
+++ index.android.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
-import { AppRegistry, StyleSheet, Text, View, TouchableHighlight } from 'react-native';
+import { AppRegistry, StyleSheet, Text, View, TouchableHighlight,PanResponder,
+    Animated,
+    Dimensions,Alert } from 'react-native';
 import ListView from './src/ListView';
 
 var RCTAudio = require('react-native-player');
 var Sound = require('react-native-sound');
+import Swiper from 'react-native-swiper';
 
 class TodoApp extends Component {
 
@@ -13,7 +16,48 @@ class TodoApp extends Component {
     this.playSoundBundle = () => {
       
     };
+
+    this.state = {
+        showDraggable   : true,     //Step 1
+        dropZoneValues  : null,
+        pan     : new Animated.ValueXY()   //Step 1
+    };
+
+    this.panResponder = PanResponder.create({    //Step 2
+        onStartShouldSetPanResponder : () => true,
+        onPanResponderMove           : Animated.event([null,{ //Step 3
+            dx : this.state.pan.x,
+            dy : this.state.pan.y
+        }]),
+        onPanResponderRelease           : (e, gesture) => {
+            if(this.isDropZone(gesture)){ //Step 1
+                this.setState({
+                    showDraggable : false //Step 3
+                });
+                Alert.alert(
+                  'Thong Bao',
+                  'Drop hoan thanh',
+                  [
+                    {text: 'Ask me later', onPress: () => console.log('Ask me later pressed')},
+                    {text: 'Cancel', onPress: () => console.log('Cancel Pressed'), style: 'cancel'},
+                    {text: 'OK', onPress: () => console.log('OK Pressed')},
+                  ],
+                  { cancelable: false }
+                );
+            }else{
+                Animated.spring(
+                    this.state.pan,
+                    {toValue:{x:0,y:0}}
+                ).start();
+            }
+        }
+    });
   }
+
+  isDropZone(gesture){     //Step 2
+    var dz = this.state.dropZoneValues;
+    return gesture.moveY > dz.y && gesture.moveY < dz.y + dz.height;
+}
 
   onPlay(){
     RCTAudio.prepare("https://noidung.tienganh123.com/file/baihoc/vocabulary/6000/bai2/famous-for-its-unique-culture.mp3", true);
@@ -21,7 +65,7 @@ class TodoApp extends Component {
   }
 
   onPlay2(){
-    const s = new Sound('advertising.mp3', Sound.MAIN_BUNDLE, (e) => {
+    const s = new Sound('for_ex3.mp3', Sound.MAIN_BUNDLE, (e) => {
         if (e) {
           console.log('error', e);
         } else {
@@ -32,11 +76,36 @@ class TodoApp extends Component {
       });
 
   }
-  render() {
-    return (
-      <View style={styles.container}>
-        
 
+  renderDraggable(){
+        //if(this.state.showDraggable){ 
+        return (
+            <View style={styles.draggableContainer}>
+                <Animated.View 
+                {...this.panResponder.panHandlers}                       
+                style={[this.state.pan.getLayout(), styles.circle]}>    
+                    
+                    <Text style={styles.text}>Drag me!</Text>
+                </Animated.View>
+            </View>
+        );
+        //}
+    }
+
+    setDropZoneValues(event){      //Step 1
+      this.setState({
+          dropZoneValues : event.nativeEvent.layout
+      });
+  }
+  render() {
+    const arr =[1,2,3];
+    var str = [];
+    for(let i = 0; i < 5; i++){
+      str.push(<View key={i} style={styles.slide2}><Text>{i}</Text></View>);
+    }
+    return (
+      <Swiper style={styles.wrapper} showsButtons={true}>
+      {/*<View style={styles.slide1}>
         <TouchableHighlight onPress={() => this.onPlay()} style={{margin: 20}}>
           <Text>Play Audio</Text>
         </TouchableHighlight>
@@ -44,21 +113,74 @@ class TodoApp extends Component {
         <TouchableHighlight onPress={() => this.onPlay2()}>
           <Text>Play Audio 2</Text>
         </TouchableHighlight>
-      </View>
+      </View>*/}
+          <View style={styles.mainContainer}>
+            <View 
+                onLayout={this.setDropZoneValues.bind(this)}     //Step 2
+                style={styles.dropZone}>
+                <Text style={styles.text}>Drop me here!</Text>
+            </View>
+
+            {this.renderDraggable()}
+        </View>
+      </Swiper>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
+let CIRCLE_RADIUS = 36;
+let Window = Dimensions.get('window');
+var styles = StyleSheet.create({
+  wrapper: {
+  },
+  slide1: {
     flex: 1,
     justifyContent: 'center',
-    paddingTop: 30,
-    paddingBottom: 10,
-    paddingLeft: 2,
-    paddingRight: 2,
-    backgroundColor: '#F8F8F8',
-  }
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  slide2: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#97CAE5',
+  },
+  slide3: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#92BBD9',
+  },
+  text1: {
+    color: '#fff',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  mainContainer: {
+        flex    : 1
+    },
+    dropZone    : {
+        height         : 100,
+        backgroundColor:'#2c3e50'
+    },
+    text        : {
+        marginTop   : 25,
+        marginLeft  : 5,
+        marginRight : 5,
+        textAlign   : 'center',
+        color       : '#fff'
+    },
+    draggableContainer: {
+        position    : 'absolute',
+        top         : Window.height/2 - CIRCLE_RADIUS,
+        left        : Window.width/2 - CIRCLE_RADIUS,
+    },
+    circle      : {
+        backgroundColor     : '#1abc9c',
+        width               : CIRCLE_RADIUS*2,
+        height              : CIRCLE_RADIUS*2,
+        borderRadius        : CIRCLE_RADIUS
+    }
 });
 
 import MainView from './main';
